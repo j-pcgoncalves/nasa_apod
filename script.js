e344d077afb324c2f1bc0c0941f8689001dbf8bc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-22 23:45:29 +0100

    update createDOMNodes

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-const createDOMNodes = () => {
-    resultsArray.forEach(result => {
+const createDOMNodes = page => {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+    console.log('Current Array', page, currentArray);
+    
+    currentArray.forEach(result => {
 
         // Card Container
         const card = document.createElement('div');
@@ -73,14 +76,13 @@ const createDOMNodes = () => {
     });
 }
 
-const updateDOM = () => {
+const updateDOM = page => {
     // Get Favorites from localStorage
     if (localStorage.getItem('nasaFavorites')) {
         favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
-        console.log(favorites);
     }
 
-    createDOMNodes();
+    createDOMNodes(page);
 }
 
 // Get 10 Images from NASA API
@@ -88,7 +90,7 @@ const getNasaPictures = async () => {
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
-        updateDOM();
+        updateDOM('favorites');
     } catch (error) {
         // Catch Error Here
     }
